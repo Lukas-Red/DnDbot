2dc5c45b037f583e9dc341746f8c44ea0320cb48
Lukas <lukas.johansson98@me.com>
2025-12-20 12:46:43 +0100

    Added query filtering to the spell parser

--- spell_parser.py
+++ spell_parser.py
@@ -13,7 +13,28 @@ class SpellBook():
 
     def get_spell(self, name):
         return self.spells.get(simplify_string(name), "Spell not found.")
+    
 
+    def get_spells_query(self, **criteria):
+        results = self.spells
+
+        for key, value in criteria.items():
+
+            if key == "level":
+                if isinstance(value, tuple):
+                    min_value, max_value = value
+                    results = {k: v for k, v in results.items() if min_value <= v.get("level", 0) <= max_value}
+                if isinstance(value, int):
+                    results = {k: v for k, v in results.items() if v.get("level", 0) == value}
+                if isinstance(value, list):
+                    results = {k: v for k, v in results.items() if v.get("level", 0) in value}
+                    
+            if key == "school":
+                if isinstance(value, str):
+                    results = {k: v for k, v in results.items() if v.get("school", "").lower() == value.lower()}
+                if isinstance(value, list):
+                    results = {k: v for k, v in results.items() if v.get("school", "").lower() in [s.lower() for s in value]}
+        
     def list_spell_keys(self):
         return [s for s in self.spells]
     
