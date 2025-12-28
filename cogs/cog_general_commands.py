c4a6ef78a8184693596dc704387be8c95c1c62b9
Lukas <lukas.johansson98@me.com>
2025-12-28 13:06:28 +0100

    Added about command

--- cogs/cog_general_commands.py
+++ cogs/cog_general_commands.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+repo_url = "https://github.com/Lukas-Red/DnDbot"
 
 class GeneralCommands(commands.Cog):
     def __init__(self, bot):
@@ -14,6 +15,10 @@ class GeneralCommands(commands.Cog):
     @commands.command()
     async def hello(self, ctx):
         await ctx.send("Hello!")
+
+    @commands.command()
+    async def about(self, ctx):
+        await ctx.send(f"Find the bot source here: {repo_url}")
 
 
 async def setup(bot):
